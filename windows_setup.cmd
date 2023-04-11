42bc4ebf90c1c415f89248801e5b32fa08465ee8
scottshireman <129428073+scottshireman@users.noreply.github.com>
2023-04-11 12:42:57 -0700

    Add files via upload

--- windows_setup.cmd
+++ windows_setup.cmd
@@ -27,29 +27,6 @@ pip install git+https://github.com/pharmapsychotic/clip-interrogator
 GOTO :eof
 
 
-REM pip install transformers==4.27.1
-pip install diffusers[torch]==0.13.0
-pip install bitsandbytes==0.35.0
-git clone https://github.com/DeXtmL/bitsandbytes-win-prebuilt tmp/bnb_cache
-pip install ftfy==6.1.1
-pip install aiohttp==3.8.3
-pip install tensorboard^>=2.11.0
-pip install protobuf==3.20.1
-pip install wandb==0.14.0
-pip install pyre-extensions==0.0.23
-pip install -U -I --no-deps https://github.com/C43H66N12O12S2/stable-diffusion-webui/releases/download/f/xformers-0.0.14.dev0-cp310-cp310-win_amd64.whl
-::pip install "xformers-0.0.15.dev0+affe4da.d20221212-cp38-cp38-win_amd64.whl" --force-reinstall
-pip install pytorch-lightning==1.6.5
-pip install OmegaConf==2.2.3
-pip install keyboard
-pip install lion-pytorch
-
-pip install open_clip_torch
-pip install salesforce-lavis
-
-
-GOTO :eof
-
 :ERROR
 echo Something blew up. Make sure Pyton 3.10.x is installed and in your PATH.
 
